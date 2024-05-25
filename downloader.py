a82b2b1f49cd026f34d78bd74bd8ec71c4ce2537
shiyida2077 <shi_yida@outlook.com>
2024-05-25 17:37:46 +0800

    add test

--- downloader.py
+++ downloader.py
@@ -2,19 +2,18 @@ import os
 import json
 from multiprocessing import Pool
 
-
-def download(videoid, path):
+def download(videoid,path):
     """
     videoid:从json读取的“videoID”列的值
     path:下载路径
     """
     # 跳过已下载的
-    if os.path.exists("%s\\%s.mp4" % (path, videoid)):
+    if os.path.exists("%s\\%s.mp4" % (path,videoid)):
         print("[DownLoaded] %s has been downloaded." % (videoid))
         return
     # 把起止时间从json中的八位秒数，转换为正常秒数，之后再转换为 xx：xx：xx的形式
     section = [int(videoid[12:18]), int(videoid[19:])]
-    print("[TimeSection]: ", section)
+    print("[TimeSection]: ",section)
     start_time = end_time = ""
     for i in range(2, -1, -1):
         start_time += "%s:" % str(section[0] // (60 ** i)).zfill(2)
@@ -23,12 +22,12 @@ def download(videoid, path):
         section[1] -= (60 ** i) * (section[1] // (60 ** i))
     start_time = start_time[:8]
     end_time = end_time[:8]
-    print("[TimeSection]: %s - %s" % (start_time, end_time))
+    print("[TimeSection]: %s - %s"% (start_time,end_time))
     # 存储参数的字典
     params = {
         "format": "bv[ext=mp4]",
         "name1": "cache\\%s.mp4" % (videoid),
-        "name2": "%s\\%s.mp4" % (path, videoid),
+        "name2": "%s\\%s.mp4" % (path,videoid),
         "start": start_time,
         "end": end_time,
         "proxy": "HTTP://127.0.0.1:7890",
@@ -39,14 +38,11 @@ def download(videoid, path):
         "yt-dlp.exe -i -f %s -o %s -R 2 --proxy %s %s"
         % (params["format"], params["name1"], params["proxy"], params["url"],))
     # 视频切割
-    # os.system(
-    #     "ffmpeg\\bin\\ffmpeg.exe -hwaccel auto -i  %s -c:v h264_qsv -ss %s -to %s -async 1 %s -y"
-    #     % (params["name1"], params["start"], params["end"], params["name2"])
-    # )
-    os.system(
-        "ffmpeg\\bin\\ffmpeg.exe -hwaccel auto -i  %s -ss %s -to %s -async 1 %s -y"
-        % (params["name1"], params["start"], params["end"], params["name2"])
-    )
+    if os.path.exists("%s" % (params["name1"])):
+        os.system(
+            "ffmpeg\\bin\\ffmpeg.exe -hwaccel auto -i  %s  -ss %s -to %s -async 1 %s -y"
+            % (params["name1"], params["start"], params["end"], params["name2"])
+        )
     # 删除原视频
     try:
         os.remove("%s" % (params["name1"]))
@@ -55,10 +51,10 @@ def download(videoid, path):
         pass
 
 
-def multi_thread(dataset, start, end, path):
+def multi_thread(dataset, start, end,path):
     for i in range(start, end):
-        print("[DownLoading]: %s.mp4\t[INDEX]: %d" % (dataset[i]['videoID'], i))
-        download(dataset[i]['videoID'], path)
+        print("[DownLoading]: %s.mp4\t[INDEX]: %d" % (dataset[i]['videoID'],i))
+        download(dataset[i]['videoID'],path)
 
 
 def cut(length, num):
@@ -77,10 +73,10 @@ def cut(length, num):
 
 if __name__ == '__main__':
 
-    params = {
-        "json": ["vatex_validation_v1.0.json", "vatex_training_v1.0.json"],
-        "path": ["val", "train"],
-        "thread_num": 32,
+    params={
+    "json":["vatex_validation_v1.0.json","vatex_training_v1.0.json","vatex_private_test_without_annotations.json","vatex_public_test_english_v1.1.json"],
+    "path":["val","train","private_test","public_test"],
+    "thread_num":16,
     }
 
     for i in range(len(params["json"])):
@@ -94,10 +90,10 @@ if __name__ == '__main__':
         length = len(dataset_json)
         spilt = cut(length, thread_num)
         print(spilt)
-        # 创建多进程池
+        #创建多进程池
         pol = Pool(thread_num)
         for j in range(thread_num):
-            pol.apply_async(multi_thread, (dataset_json, spilt[j], spilt[j + 1], params["path"][i]))
+            pol.apply_async(multi_thread,(dataset_json,spilt[j],spilt[j+1],params["path"][i]))
         pol.close()
         pol.join()
         del pol
